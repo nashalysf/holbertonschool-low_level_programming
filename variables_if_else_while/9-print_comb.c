0a7c06c957b34f998aaf554408476446263d92e9
nashalysf <nashalysf@gmail.com>
2023-02-20 21:32:57 +0000

    print me single digits

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	int x;
+	for(x = '0'; x <= '9'; x++){
+		putchar(x);
+		if(x < '9'){
+			putchar(x);
+			putchar(',');
+		} 
+		putchar('\n');
+		return(0);
+	}
+}
