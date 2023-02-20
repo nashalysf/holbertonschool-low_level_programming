53e59eb3300649e1b2f037f3b5bd667c336408b6
nashalysf <nashalysf@gmail.com>
2023-02-20 17:35:27 +0000

    my first source file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /*
- * main
- * return
+ * main - Entry
+ * return - zero for success
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return(0);
 
 }
