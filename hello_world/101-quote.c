57299ec562e6e223c439bd5f37306c1f04d0a978
nashalysf <nashalysf@gmail.com>
2023-02-20 18:14:53 +0000

    quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 
 	return(1);
-
 }
