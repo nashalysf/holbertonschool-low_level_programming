5bf3601f7b023ebaafdf855b21a7994b65b6d0e0
nashalysf <nashalysf@gmail.com>
2023-02-20 20:41:05 +0000

    print me the alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	char x,  q, e;
+
+	q = 'q';
+	e = 'e';
+
+	for(x 'a'; x <= 'z'; x++){
+		if(x != q || x! = e){
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return(0);
+
+}
