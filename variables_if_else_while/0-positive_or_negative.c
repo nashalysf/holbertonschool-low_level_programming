1b91b148b6ed8a1137c892d1d31bff427205fb5b
nashalysf <nashalysf@gmail.com>
2023-02-21 15:22:53 +0000

    fixing betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- * Return: Always 0. (Success)
- */
+*main - Entry point
+*
+*Description: checks the remaining value of the condition,
+*and executes with controllers.
+*Return: Always 0. (Success)
+*/
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	} else if(n == 0)
+	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	} else
