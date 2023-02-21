98d7eff4ef085c467b58c8ac74303f6c35405938
nashalysf <nashalysf@gmail.com>
2023-02-21 15:33:28 +0000

    fixing betty

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+*main - Entry point
+*
+*Description: prints string.
+*Return: Always 0. (Success)
+*/
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n");
 
-	return(1);
+	return (1);
 }
