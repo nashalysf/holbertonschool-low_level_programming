f5747eab8d74fedd1a411ceab4ee4aecb4c372f8
nashalysf <nashalysf@gmail.com>
2023-02-21 14:45:00 +0000

    fixing betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /*
- *main - Entry point
- *Description: 'checks the remaining value of the condition and executes with controllers'
- *Return: Always 0. Success
- */
+*main - Entry point
+*Description: checks the remaining value of the condition and executes with controllers
+*Return: Always 0. Success
+*/
 int main(void)
 {
 	int n;
@@ -13,7 +13,6 @@ int main(void)
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-
 	if (n % 10 > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
