16a0fff424d2909d50fabf47ffedc59891cc16c5
nashalysf <nashalysf@gmail.com>
2023-02-26 23:14:20 +0000

    print absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*int _abs - Entry point
+*
+*Description: computes absolute value.
+*Return: 0. (Success)
+*/
+int _abs(int r){
+if (r >= 0)
+{
+return (0);
+}
+return (r * -1);
+}
