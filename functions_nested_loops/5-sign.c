a594f489fe516c6cb199e49f628032fcabed778c
nashalysf <nashalysf@gmail.com>
2023-02-26 22:39:04 +0000

    check sign of number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*print_sign - Entry point
+*@n: input param
+*Description: checks if number is positive,
+*zero or negative
+*Return: 0.(Success)
+*/
+int print_sign(int n){
+char x;
+if (n > 0)
+{
+x = '+';
+_putchar(x);
+}
+else if (n == 0)
+{
+x = '0';
+_putchar(x);
+}
+else if (n < 0)
+{
+x = '-';
+_putchar(x);
+}
+return (0);
+}
