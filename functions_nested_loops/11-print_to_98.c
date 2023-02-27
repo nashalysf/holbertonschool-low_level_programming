27dc3c35a783a3755b3365028c74dcefb52cd407
nashalysf <nashalysf@gmail.com>
2023-02-27 01:26:55 +0000

    print natural numbers

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* - Entry point
-*
-*Description: .
-*Return: (Success)
+*print_to_98- Entry point
+*@n: input param
+*Description: prints all natural
+*numbers from n to 98.
+*Return: 0. (Success)
 */
+void print_to_98(int n){
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+printf("%d", n);
+if (n == 98)
+continue;
+printf(", ");
+}
+putchar('\n');
+}
+else
+{
+for (; n >= 98; n--)
+{
+printf("%d", n);
+if (n == 98)
+continue;
+printf(", ");
+}
+putchar('\n');
+}
+return;
+}
