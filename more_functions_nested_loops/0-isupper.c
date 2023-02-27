fe5021caedf6c0631a9c5d82880578e197468d14
nashalysf <nashalysf@gmail.com>
2023-02-27 17:22:33 +0000

    check for uppercase

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
 *_isupper- Entry point
-*
+*@c: input param
 *Description: checks for uppercase char
 *Return: 0. (Success)
 */
 int _isupper(int c)
 {
 	char x;
-	for(x = 'A'; x <= 'Z'; x++){
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+
 	if (x == c)
 	{
+
 	return (1);
 	}
 	}
