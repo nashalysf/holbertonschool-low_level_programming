9127366b372303a79f6b00929636a913f2b35709
nashalysf <nashalysf@gmail.com>
2023-02-27 17:36:35 +0000

    print if digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isdigit- Entry point
+*@c: input param
+*Description: checks for a digit.
+*Return: 0. (Success)
+*/
+int _isdigit(int c){
+char x;
+	for (x = '0'; x <= '9'; x++){
+	if (x == c)
+	{
+		return (1);
+	}
+	}
+	return (0);
+}
