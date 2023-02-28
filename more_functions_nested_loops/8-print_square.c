97651bfd2c3d777e275cdc38cb98366b080db8c4
nashalysf <nashalysf@gmail.com>
2023-02-28 15:03:01 +0000

    prints square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_square- Entry point
+*@size: input param
+*Description: prints square
+*Return: 0. (Success)
+*/
+void print_square(int size)
+{
+int x = 0;
+if (size > 0)
+{
+	for(; x < size; x++)
+	{
+	_putchar('#');
+	}
+}
+_putchar('\n');
+}
+
