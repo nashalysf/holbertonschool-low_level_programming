982ec45c26bd70ea27f3720d6b07a4f1a5cdf5be
nashalysf <nashalysf@gmail.com>
2023-02-28 14:37:18 +0000

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_diagonal- Entry point
+*@n: number of times char prints
+*Description:
+*Return: 0. (Success)
+*/
+void print_diagonal(int n)
+{
+int x = 0;
+if (n > 0)
+{
+for (; x < n; x++)
+{
+_putchar('\\');
+}
+}
+_putchar('\n');
+}
