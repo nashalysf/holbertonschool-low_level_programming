a37d29437eaffe01ac1f0565bf81468d45d0309d
nashalysf <nashalysf@gmail.com>
2023-02-28 16:22:42 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ for (x = 1; x < 101; x++)
 	if ((x % 15) == 0 && x != 1)
 		printf("FizzBuzz ");
 	
-	else if ((x % 10) == 5)
+	else if ((x % 5) == 0)
                 printf("Buzz ");
 		
 
-	else if ((x % 10) == 3)
+	else if ((x % 3) == 0)
 		printf("Fizz ");
 
 	else
