3d730911d61caf3c27211fc21a60c6461114d6ae
nashalysf <nashalysf@gmail.com>
2023-03-01 21:05:00 +0000

    show me the length

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_puts- Entry point
+*@str: string being printed
+*Description: prints a string
+*Return: 0. (Success)
+*/
+void _puts(char *str)
+{
+	int i;
+char string[15] = *str;
+for (i = 0; i < 15; i++)
+{
+	_putchar(string[i]);
+}
+}
