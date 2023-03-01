6d4e2d090b2d320d22c58b0695865ae6be216e50
nashalysf <nashalysf@gmail.com>
2023-03-01 19:52:57 +0000

    pointer 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*reset_to_98- Entry point
+*@n: pointer param
+*Description: takes pointer to an
+*int as param and updates value
+*Return: 0. (Success)
+*/
+void reset_to_98(int *n)
+{
+int* ptr = n;
+*ptr = 98;
+_putchar(*ptr);
+}
