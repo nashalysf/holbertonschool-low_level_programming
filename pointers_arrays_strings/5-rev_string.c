0bacdaac3da5cb8ba9155a7dd0087ea15a173c87
nashalysf <nashalysf@gmail.com>
2023-03-01 22:07:50 +0000

    reverse function

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,22 @@
 #include <string.h>
 #include "main.h"
 /**
-*- Entry point
-*
-*Description: 
+*rev_string- Entry point
+*@s: string param
+*Description: function to reverse
+*a string
 *Return: 0. (Success)
 */
 void rev_string(char *s)
 {
 int i = 0;
 int x = 0;
+
 while (s[i] != '\0')
 	i++;
 while (x < i--)
 {
 	char y = s[i];
 	_putchar(y);
-
 }
 }
