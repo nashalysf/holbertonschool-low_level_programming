cd652480b8c856921460e14cdb8a39c2eb41f865
nashalysf <nashalysf@gmail.com>
2023-03-02 19:51:51 +0000

    print array elemnts

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_array- Entry point
+*@a: array param
+*@n: size of array
+*Description: prints each elemnt 
+*that the array holds
+*Return: 0. (Success)
+*/
+void print_array(int *a, int n)
+{
+int *array = a;
+int len = n;
+int i = 0;
+
+for (; i < len; i++)
+	printf("%d, \n", array[i];
+_putchar('\n');`}
