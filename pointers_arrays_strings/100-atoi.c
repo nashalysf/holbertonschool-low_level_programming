8b42572bd436b8bbf7dbbe5d25d91fdfe9f58511
nashalysf <nashalysf@gmail.com>
2023-03-05 23:01:31 +0000

    converts string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,11 +16,12 @@ while (*s++)
 {
 if (*s == '-' && *s == '+')
 	morp *= -1;
-if (*s >= '0' && *s <= '9')
+
+else if (*s >= '0' && *s <= '9')
 	i = (i * 10) + (*s - '0');
 
-if (i > 0)
+else if (i > 0)
 	break;
-	}
+}
 	return (i * morp);
 }
