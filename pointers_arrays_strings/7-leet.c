a318b1fb34f8f22bda6f5d2e98b1e3745df7b726
nashalysf <nashalysf@gmail.com>
2023-03-06 02:51:35 +0000

    change letters to numbers

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - Entry point
+ * @l: the string to be converted into leet speak
+ *
+ * Description: converts sting into leet
+ * Return: char
+ */
+char *leet(char *l)
+{
+	int i = 0;
+	int x = 0;
+	char *c = "aAeEoOlLtT";
+	char *n = "4433001177";
+
+	while (*(l + i) != '\0')
+	{
+		while (*(c + x) != '\0')
+		{
+			if (*(l + i) == *(c + x))
+			{
+				*(l + i) = *(n + x);	
+			}
+		x++;
+		}
+			x = 0;
+			i++;
+	}
+	return (l);
+}
