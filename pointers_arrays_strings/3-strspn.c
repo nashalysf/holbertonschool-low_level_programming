54e09dc1bb958082eca6a31eab218480ce90c8d0
nashalysf <nashalysf@gmail.com>
2023-03-06 21:52:54 +0000

    gets length of substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+*int _strspn- Entry point
+*@s: bytes from accept
+*@accept: initial segment
+*Description: gets length of substring
+*Return: number of bytes
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i= 0;
+	int torf;
+	char *initial = accept;
+
+	while (*s)
+	{
+		torf = 0;
+	while (*accept)
+	{
+		if (*accept == *s)
+		{
+			i++;
+			torf = 1;
+			break;
+		}
+		accept++;
+	}
+	s++;
+	accept = initial;
+	if (torf == 0)
+	break;
+	}
+
+	return (i);
+}
