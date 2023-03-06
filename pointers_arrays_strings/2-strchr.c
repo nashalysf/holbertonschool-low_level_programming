3385b908b94e89ebc71bb2d44f833aefe8679796
nashalysf <nashalysf@gmail.com>
2023-03-06 21:35:07 +0000

    locates a char in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_strchr- Entry point
+*@s: string param
+*@c: character param
+*Description: locates a char or
+*null in a string
+*Return: char
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	if (c == s[i])
+		return (s);
+}
+	if (c == s[i])
+		return (s);
+
+return ('\0');
+}
