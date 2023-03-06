b099a739efcedbbd0162de89979447b76bb8c116
nashalysf <nashalysf@gmail.com>
2023-03-06 22:01:13 +0000

    searches a string for bytes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strpbrk - searches a string for a set of bytes
+* @s: string param
+* @accept: char param
+* Return: pointer to the byte that matches accept
+* or NULL
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	char *initial = accept;
+
+	while (*s)
+	{
+		while (*accept)
+		{
+			if (*accept == *s)
+			return (s);
+			accept++;
+		}
+	accept = initial;
+	s++;
+	}
+
+	return (NULL);
+}
