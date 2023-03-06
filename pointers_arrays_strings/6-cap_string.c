353c7ca5de8879c9aba332953f220262f86dfede
nashalysf <nashalysf@gmail.com>
2023-03-06 02:20:56 +0000

    capitalize string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,21 +2,17 @@
 /**
 *cap_string- Entry point
 *@str: string param
-*Description: changes all lowercase
-*to uppercase by substracting from
-*ascii table
+*Description: capitalize letters
+*from string
 *Return: -32. char
 */
 char *cap_string(char *str)
 {
 char *s = str;
 
-while (*s)
+for (*s = 'a'; *s <= 'z'; s++)
 {
-        if (*s >= 'a' && *s <= 'z')
-                *s -= 32;
-
-        s++;
+	*s -= 32;
 }
 return (s);
 }
