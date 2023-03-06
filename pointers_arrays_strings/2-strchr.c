0d01a136fcaedb7b333d4fa9f664013653a8b9be
nashalysf <nashalysf@gmail.com>
2023-03-06 21:36:39 +0000

    locates a char in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,11 @@ char *_strchr(char *s, char c)
 {
 int i;
 
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0';)
 {
 	if (c == s[i])
 		return (s);
+	i++;
 }
 	if (c == s[i])
 		return (s);
