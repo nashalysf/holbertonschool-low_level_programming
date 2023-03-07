21ab29a597ca7703c7843e65ec342884d9eacada
nashalysf <nashalysf@gmail.com>
2023-03-07 19:04:40 +0000

    print length of string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion- Entry point
+ * @s: string param
+ * Description: return length of string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+int i = 0;
+
+if (*s)
+{
+	i = s[i];
+}
+	return (i);
+}
