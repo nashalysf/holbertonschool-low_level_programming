ac6425e4fe64310d7ddf7ece5ec3cbc7f1046086
nashalysf <nashalysf@gmail.com>
2023-03-07 00:20:59 +0000

    prints the sum of 2 square matrix of int

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,12 +14,12 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 = 0;
-		sum1 = sum1 + a[(i * size) + i];
+		sum1 = sum1 + a[i * size + i];
 	}
 	for (i = 1; i <= size; i++)
 	{
 		sum2 = 0;
-		sum2 = sum2 + a[(i * size) - i];
+		sum2 = sum2 + a[i * size - i];
 }
 printf("%d, %d\n", sum1, sum2);
 }
