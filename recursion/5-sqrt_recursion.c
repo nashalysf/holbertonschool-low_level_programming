a848c4ebc10e79c3d924b70c984be1049a17443b
nashalysf <nashalysf@gmail.com>
2023-03-08 19:56:22 +0000

    get square root

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -26,10 +26,10 @@ else
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-        return (-1);
+	return (-1);
 if (n == 0)
-        return (1);
+	return (1);
 else
-        return get_sqrt(n, 1);
+	return (get_sqrt(n, 1));
 }
 
