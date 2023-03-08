ec6af2c8e7b7c3a1a62abc71b158b2ec6d0090c4
nashalysf <nashalysf@gmail.com>
2023-03-08 19:54:41 +0000

    get square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * get_sqrt- Entry point
+ * @n: current value param
+ * @i: traverser param
+ * Description: gets square root
+ * Return: square root
+ */
+int get_sqrt(int n, int i)
+{
+if (n == (i * i))
+	return (i);
+else if (n > (i * i))
+	return (get_sqrt(n, i + 1));
+else
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion- Entry point
+ * @n: num param
+ * Description: returns the natural square root
+ * of n
+ * Return: return new value
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+        return (-1);
+if (n == 0)
+        return (1);
+else
+        return get_sqrt(n, 1);
+}
+
