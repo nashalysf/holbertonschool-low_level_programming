5ca2fc2968a6bac3a6a905a3c7b42a35fc95b9ec
nashalysf <nashalysf@gmail.com>
2023-03-09 16:46:50 +0000

    print quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,7 +11,6 @@ int main(void)
 {
 	char quote[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	write(2, quote, 59);
-
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
