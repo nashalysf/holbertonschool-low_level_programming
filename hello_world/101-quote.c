f5efc195e12cd95014d30d1d94055cc73abfc171
nashalysf <nashalysf@gmail.com>
2023-03-09 16:10:30 +0000

    print quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\"
-			- Dora Korpar, 2015-10-19\n");
+	fwrite(STDOUT_FILENO, "and that piece of art is useful\""
+			"- Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
