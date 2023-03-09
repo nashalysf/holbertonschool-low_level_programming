cf2d1264d27792edb7810a30a894980ebbfd20a5
nashalysf <nashalysf@gmail.com>
2023-03-09 16:28:26 +0000

    print quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -4,13 +4,14 @@
 *main - Entry point
 *
 *Description: prints string.
-*Return: Always 0. (Success)
+*Return: 1. (Success)
 */
 
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful\""
-			"- Dora Korpar, 2015-10-19\n", 59);
+	char quote[] = "\"and that piece of art is useful\"""- Dora Korpar, 2015-10-19\n";
+	
+	fwrite(2, quote, 59);
 
 	return (1);
 }
