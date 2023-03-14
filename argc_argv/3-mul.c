f9b27fd8bd11450abefabf28841911e36eda18c5
nashalysf <nashalysf@gmail.com>
2023-03-14 18:58:36 +0000

    multiply args

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,13 +12,18 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int mul = 1;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 if (argc > 1)
 {
 	for (i = 1; i < argc; i++)
 {
 	mul *= atoi(argv[i]);
-	printf("%d\n", mul);
 }
+	printf("%d\n", mul);
 }
 return (0);
 }
