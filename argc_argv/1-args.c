b455eebade9dc7e9c2d10883060dd3b5ef310b66
nashalysf <nashalysf@gmail.com>
2023-03-14 18:23:05 +0000

    print num of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main- entry point
+ * @argc: arg count
+ * argv: arg array
+ * Description: prints number of args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+if (argv[0])
+	printf("%d\n", argc);
+return (argc);
+}
