635bdd767fca2039d789366428c5fec825a72e04
nashalysf <nashalysf@gmail.com>
2023-03-14 18:48:23 +0000

    multiply arguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main- entry point
+ * @argc: arg count
+ * @argv: arg array
+ * Description: multiplies args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+if (argc > 1)
+{
+	for (i = 1; i < argc; i++)
+{
+	mul *= atoi(argv[i]);
+	printf("%d\n", mul);
+}
+}
+return (0);
+}
+
