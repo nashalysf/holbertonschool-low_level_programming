85a0c7704115723804b1d4496614427b2c70e7bb
nashalysf <nashalysf@gmail.com>
2023-03-20 07:03:47 +0000

    memory allocation

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * malloc_checked - entry point
+ * @b: var to be allocated
+ * Description: allocates memory
+ * Return: pointer to mem
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);  
+	
+	return (ptr);
+}
