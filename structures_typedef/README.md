770b2ea0a578341b315943ecb30d8ca1488f9891
nashalysf <nashalysf@gmail.com>
2023-03-21 15:03:46 +0000

    my first structure

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+read me file typedefs and structures
---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - structure / obj prototype
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owners name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
