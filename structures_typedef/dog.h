4bb513520fe720bb6582e30f285e5dd7a50f4500
nashalysf <nashalysf@gmail.com>
2023-03-21 15:31:12 +0000

    h update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,3 +13,12 @@ float age;
 char *owner;
 };
 #endif
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**void print_dog(struct dog *d);
+
+*dog_t *new_dog(char *name, float age, char *owner);
+
+*void free_dog(dog_t *d);
+*/
