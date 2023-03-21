bbf3b6a367cc562d6e4704ea993e44caf9b4dff5
nashalysf <nashalysf@gmail.com>
2023-03-21 15:30:20 +0000

    var of type struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * init_dog - entry point
+ * @dog: dog structure
+ * @d: address of struct
+ * @name: dogs name
+ * @age: dogs name
+ * @owner: owners name
+ * Description: inits a var of type struct dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
