d34710e87a3778bc066ba30666fb95979accb054
nashalysf <nashalysf@gmail.com>
2023-03-23 23:56:37 +0000

    executes func with param

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  -
+ *@ :
+ *Return:
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+	else
+	{
+		while (i < size)
+		{
+		action(array[i]);
+		i++;
+		}
+	}
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,14 +1,14 @@
 #ifndef _FUNC_PTRS_
 #define _FUNC_PTRS_
+#include <stddef.h> 
 void print_name(char *name, void (*f)(char *));
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 /*
- * void array_iterator(int *array, size_t size, void (*action)(int));
-*
 * int int_index(int *array, int size, int (*cmp)(int));
-*
+* /
 * int op_add(int a, int b);
-*
+* /
 * int (*get_op_func(char *s))(int, int);
 */
 int _putchar(char c);
