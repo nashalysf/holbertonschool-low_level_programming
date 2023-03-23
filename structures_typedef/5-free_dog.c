421153f1762b05d637cd47d4d5bf88d1738a23a8
nashalysf <nashalysf@gmail.com>
2023-03-23 21:13:04 +0000

    free struct memory

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dogs- free struct memory
+ * @d: memory to be freed
+ * Return: freed memory
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
