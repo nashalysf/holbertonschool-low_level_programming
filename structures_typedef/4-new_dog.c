8966891f5ba21f8d8b42cfad4acf0392ef623f4e
nashalysf <nashalysf@gmail.com>
2023-03-23 21:06:31 +0000

    create a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 
 	new_dog->name = malloc(strlen(name) + 1);
 	new_dog->owner = malloc(strlen(owner) + 1);
