a8906e5e2e3ed80fd0a632e3b0cb1d235a406d29
nashalysf <nashalysf@gmail.com>
2023-03-23 20:51:09 +0000

    changing macro to typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -30,7 +30,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  */
 void print_dog(struct dog *d);
 
-#define dog_t struct dog
+/**
+ * dog_t: new name od struct dog
+ */
+typedef struct dog dog_t;
 
 /**
  * new_dog - entry point
