191a19a853ffa492718fdd0df5c75536e7af39ab
nashalysf <nashalysf@gmail.com>
2023-03-24 16:53:13 +0000

    fixing quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,7 +11,8 @@ int main(void)
 {
 	int i = 0;
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	while(quote[i])
+
+	while (quote[i])
 	{
 		putchar(quote[i]);
 		i++;
