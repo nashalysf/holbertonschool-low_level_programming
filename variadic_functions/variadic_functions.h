b2c46dd4c1708c02d65634e6ac8bf39d9dbd81e8
nashalysf <nashalysf@gmail.com>
2023-03-24 02:17:59 +0000

    fixing h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,11 @@
 #ifndef _VAR_FUNC_
-#define VAR_FUNC_
+#define _VAR_FUNC_
+
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
+
 #include <stdarg.h>
 typedef va_list var;
+
 #endif
