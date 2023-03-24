919cc9741bf0add5d21bc9cac3f0bf160eed9da2
nashalysf <nashalysf@gmail.com>
2023-03-24 02:57:46 +0000

    prints string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,14 +19,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(ap, char *);
 
-		if (separator == NULL)
-			continue;
-
 		if (string == NULL)
 			printf("(nil)");
 
 		printf("%s", string);
-		if (i < n - 1)
+		if (separator == NULL)
+			continue;
+		else (i < n - 1)
 			printf("%s", separator);
 		}
 
