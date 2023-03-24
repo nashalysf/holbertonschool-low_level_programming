82a19dfe6d3ec9047952f715370cb8c33957963a
nashalysf <nashalysf@gmail.com>
2023-03-24 17:07:39 +0000

    prints n dimensions table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_times_table - prints n times
+ * @n: table dimensions
+ * Return: 0. Success
+ */
+void print_times_table(int n)
+{
+	int i, x;
+
+	if (n < 15 || n > 0)
+	{
+	for (i = 0; i <= n; i++)
+	{
+		putchar('0');
+		for (x = 0; x <= n; x++)
+			printf("%d\n", i * x);
+	}
+	}
+}
