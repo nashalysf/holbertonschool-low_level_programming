c4bd73b3ec6d14ee6c026d8bc5e439845bae3341
nashalysf <nashalysf@gmail.com>
2023-03-24 02:55:59 +0000

    prints string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_strings- prints string
+ * @separator: string in between
+ * @n: number of ints passed
+ * Return: numbers
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	var ap;
+	char *string;
+	unsigned int i = 0;
+
+	va_start(ap, n);
+
+	for (; i < n; i++)
+	{
+		string = va_arg(ap, char *);
+
+		if (separator == NULL)
+			continue;
+
+		if (string == NULL)
+			printf("(nil)");
+
+		printf("%s", string);
+		if (i < n - 1)
+			printf("%s", separator);
+		}
+
+		printf("\n");
+		va_end(ap);
+}
