0cf204e5e216a244db893d691d78817b3f14fd6c
nashalysf <nashalysf@gmail.com>
2023-03-30 13:37:03 +0000

    calculator

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s)) (int, int)
 	{NULL, NULL}
 	};
 	int i = 0;
-	
+
 	while (i < 5)
-	{	
+	{
 		if (*(ops[i]).op == *s && *(s + 1)  == '\0')
 			return (ops[i].f);
 		i++;
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,19 +4,19 @@
 #include <string.h>
 /**
  * main - entry point
- * argc: arg count
- * argv: arg array
+ * @argc: arg count
+ * @argv: arg array
  * Return: arithmetic result
- */ 
-int main (int argc, char *argv[])
+ */
+int main(int argc, char *argv[])
 {
 
-	int (*op_func)(int, int);	
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 
 	op_func = get_op_func(argv[2]);
@@ -24,7 +24,7 @@ int main (int argc, char *argv[])
 	if (op_func == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
 	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
