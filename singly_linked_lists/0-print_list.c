f6d4ea1a07f9ab4d4dd6840297b003393cc34673
nashalysf <nashalysf@gmail.com>
2023-04-04 20:49:34 +0000

    print and return amount of nodes

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - prints all elmnts of list_t
+ * @h: 
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+	while (h != NULL)
+	{		
+	       	if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+		printf("%u %s", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
