ab74c1013c2e4af0c24bba2de37483837723dc10
nashalysf <nashalysf@gmail.com>
2023-04-04 21:14:35 +0000

    print amount of elemnt

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * list_len - returns number of elmnts in list_t
+ * @h: pointer to head node
+ * Return: number elmnts
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
