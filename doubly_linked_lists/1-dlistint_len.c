5928aa9addd5c79f0b459f82db7eb156a0c2787f
nashalysf <nashalysf@gmail.com>
2023-04-11 20:24:21 +0000

    returns amount of elements on list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns num of elements on list
+ * @h: pointer to head node
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+		{
+			i++;
+			h = h->next;
+		}
+
+	return (i);
+}
