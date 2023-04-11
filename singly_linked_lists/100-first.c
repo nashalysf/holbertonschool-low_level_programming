705f2af170b38aa7b15600eb4876d191e4e6b8ba
nashalysf <nashalysf@gmail.com>
2023-04-11 19:14:34 +0000

    prints string before main is executed with attribute constructor as prototype

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print - a function that prints before main
+ * Return: void
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}	
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -32,4 +32,6 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 int _putchar(char c);
+
+void print(void) __attribute__((constructor));
 #endif
