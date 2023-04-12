2a85de22e9bee8d15ee5aac8b9b44bdf0a0fe052
nashalysf <nashalysf@gmail.com>
2023-04-12 04:49:22 +0000

    sums elements on list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sums all data of lists
+ * @head: ptr to head node
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
