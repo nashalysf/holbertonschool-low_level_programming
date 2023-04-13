b1919da0a0498b5fccc19a35180a6fb513f2ccc6
nashalysf <nashalysf@gmail.com>
2023-04-13 23:25:11 +0000

    adding libraries

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,16 @@
+/*GUARD*/
 #ifndef _FILE_
 #define _FILE_
+
+/*LIBRARIES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdarg.h>
+
+/*PROTOTYPES*/
 int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
