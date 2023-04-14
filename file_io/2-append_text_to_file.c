be6c2e0488091a4a6a81c36a2dee98d82b64183b
nashalysf <nashalysf@gmail.com>
2023-04-14 00:48:50 +0000

    appends text to a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	buffer = malloc(sizeof(char) * text_write_des);
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	file_des = open(filename, O_WRONLY | O_APPEND);
