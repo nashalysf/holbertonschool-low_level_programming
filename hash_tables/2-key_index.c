fcc587322bd313772e8a5a80f766894f6674d9ca
nashalysf <nashalysf@gmail.com>
2023-04-14 17:26:59 +0000

    returns index value of key

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives the index of key
+ * @key: key value
+ * @size: size of array
+ * Return: index of key/value pair
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned int i = 0;
+
+	while (key)
+	{
+
+	if (key[i] == size)
+		break;
+	i++;
+
+	}
+	return (i);
+}
