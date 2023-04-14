31a737109f5bd8b5b2ad6d5505fc29605cf41848
nashalysf <nashalysf@gmail.com>
2023-04-14 16:45:37 +0000

    fixing alloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,10 +9,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 
 	if (size > 0)
-		new_table = malloc(sizeof(char) * size);
-
-	if (!new_table)
-		return (NULL);
+	{
+		new_table = malloc(sizeof(hash_table_t));
+		if (!new_table)
+			return (NULL);
+		new_table->size = size;
+		new_table->array = malloc(sizeof(char *) * size);
+		if (!new_table->array)
+			return (NULL);
+	}
 
 	return (new_table);
 }
