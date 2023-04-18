80226aa75ed715af6b4f6ff716bb353a2acbdde0
nashalysf <nashalysf@gmail.com>
2023-04-18 23:27:15 +0000

    prints hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,26 +6,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *head;
-	char *aux;
+unsigned long int i = 0, last_pos = 0;
+	hash_node_t *node = NULL;
 
-
-	if (!ht)
+	if (ht == NULL)
 		return;
-
-	printf("{");
-	aux = "";
-
-	for (i = 0; i < ht->size; i++)
+	putchar('{');
+	if (ht)
 	{
-		head = ht->array[i];
-
-		while (head != NULL)
+		for (; i < ht->size - 1; i++)
 		{
-			printf("%s'%s: '%s'", aux, head->key, head->value);
-			aux = ", ";
-			head = head->next;
+			if (ht->array[i] != NULL)
+				last_pos = i;
+		}
+
+		for (i = 0; i <= last_pos; i++)
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (i < last_pos - 1)
+					printf(", ");
+			}
+
 		}
 	}
 	printf("}\n");
