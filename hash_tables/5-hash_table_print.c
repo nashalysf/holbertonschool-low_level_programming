afa1e2596060a7fb62800c4d3df0fff065ebf810
nashalysf <nashalysf@gmail.com>
2023-04-18 23:11:06 +0000

    prints hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table to print
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, aux = 0;
+	hash_node_t *head;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+
+		while (head)
+		{
+			if (aux != 0)
+				printf(", ");
+			printf("%s': '%s'", head->key, head->value);
+			head = head->next;
+			aux++;
+		}
+	}
+	printf("}\n");
+}
