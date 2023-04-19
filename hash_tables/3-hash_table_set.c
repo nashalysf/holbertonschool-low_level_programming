da8e117f5e72b378aeab68eeea7c6b5d3737bff6
nashalysf <nashalysf@gmail.com>
2023-04-19 14:19:52 +0000

    adds elmnt to hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,34 +9,47 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *dup_k, *dup_v;
-	hash_node_t *dup_n;
+	hash_node_t *dup_n, *tmp_n;
 	unsigned int i;
 
-	dup_n = malloc(sizeof(hash_node_t));
-
-	if (!dup_n)
+	if (!ht | !key | !value)
+		return (0);
+	else if (strlen(key) == 0)
 		return (0);
 
 	dup_k = strdup(key);
 	dup_v = strdup(value);
-
-	if (!ht | !key | !value)
+	dup_n = malloc(sizeof(hash_node_t));
+	if (!dup_n)
 		return (0);
 
-	i = hash_djb2((const unsigned char *)dup_k) % (ht->size);
+	i = hash_djb2((const unsigned char *)key) % (ht->size);
 	dup_n->key = dup_k;
 	dup_n->value = dup_v;
 	dup_n->next = NULL;
 
-	if (ht->array[i] && strcmp(ht->array[i]->key, dup_k) != 0)
-		node->next = ht->array[i];
-	else if (ht->array[i])
+	if ((ht->array)[i] != NULL)
 	{
-		free(ht->array[i]->key);
-		free(ht->array[i]->value);
-		free(ht->array[i]);
-	}
+		tmp_n = (ht->array)[i];
+		while (tmp_n)
+		{
+			if (strcmp(tmp_n->key, dup_k) == 0)
+			{
+				free(ht->array[i]->value);
+				ht->array[i]->value = dup_v;
+				free(dup_k);
+				free(dup_n);
+				return (1);
+			}
 
-	ht->array[i] = dup_n;
+			tmp_n = tmp_n->next;
+		}
+		tmp_n = (ht->array)[i];
+		dup_n->next = tmp_n;
+		(ht->array)[i] = dup_n;
+	}
+	else
+		(ht->array)[i] = dup_n;
+
 	return (1);
 }
