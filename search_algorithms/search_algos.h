4337e92b8f0a8f16bfb84585595cd5c3867241c1
nashalysf <nashalysf@gmail.com>
2023-05-29 19:55:49 +0000

    pushing header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_
+#define SEARCH_ALGOS_
+/*----------LIBRARIES----------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*----------PROTOTYPES----------*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
